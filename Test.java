cf316f1ff485d033e5ed5f602961e32f97666949
Emanuel <emanuel.orler@gmail.com>
2020-09-27 18:31:05 -0300

    Requerimiento: Eliminar Test

--- Test.java
+++
@@ -1,8 +0,0 @@
-package proyecto_1;
-
-public class Test {
-	public static void main(String[]args) {
-		OperatingSystem newLinux = new OperatingSystem("Linux", 0001);
-		System.out.println(newLinux.getName() +" id is: "+newLinux.getId());
-	}
-}
